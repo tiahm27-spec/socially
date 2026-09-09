04822029ffbe26e2a0444eee7b752f50e733016b
tiahm27-spec <tiahm27@icstudents.org>
2026-09-09 13:52:35 -0500

    Update └── app.js

--- └── app.js
+++ └── app.js
@@ -7,3 +7,42 @@ function showPage(pageName) {
 
   document.getElementById(pageName).classList.add("active");
 }
+
+
+// Create a new post
+const postButton = document.querySelector(".post-buttons button:last-child");
+const postBox = document.querySelector("textarea");
+
+postButton.addEventListener("click", function () {
+  const text = postBox.value.trim();
+
+  if (text === "") {
+    alert("Write something first!");
+    return;
+  }
+
+  const newPost = document.createElement("div");
+  newPost.className = "card post";
+
+  newPost.innerHTML = `
+    <div class="post-user">
+      <div class="avatar">M</div>
+      <div>
+        <strong>mari</strong>
+        <p>@mari</p>
+      </div>
+    </div>
+
+    <p>${text}</p>
+
+    <div class="reactions">
+      ❤️ Like
+      💬 Comment
+      ↗️ Share
+    </div>
+  `;
+
+  document.getElementById("home").appendChild(newPost);
+
+  postBox.value = "";
+});
